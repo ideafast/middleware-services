c9f2db7048b19eee77fbd68cc8f90595f7772826
Drew Miley <drew.miley@newcastle.ac.uk>
2020-10-13 13:25:17 +0100

    Runs print time job in separate file

--- data_transfer/main.py
+++ data_transfer/main.py
@@ -1,24 +1,22 @@
 import uvicorn
 import logging
-import time
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 
-logger = logging.getLogger(__name__)
-app = FastAPI()
-counter = 0
+from .jobs import example
 
-@app.on_event("startup")
+logger = logging.getLogger(__name__)
+data_transfer = FastAPI()
+
+
+@data_transfer.on_event("startup")
 @repeat_every(seconds=1, logger=logger, wait_first=True)
-def periodic():
-    global counter
-    print('counter is', counter)
-    counter += 1
-    # Run file
+def print_time():
+    example.run_job()
 
 def main():
     uvicorn.run(
-        "data_transfer.main:app",
+        "data_transfer.main:data_transfer",
         host="0.0.0.0",
         port=8001,
         reload=True)
