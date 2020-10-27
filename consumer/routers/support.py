bc2425b888c7a986c08fdde1f7622ba58e010107
Drew Miley <drew.miley@newcastle.ac.uk>
2020-10-27 16:25:13 +0000

    Returns users from zammad api

--- consumer/routers/support.py
+++ consumer/routers/support.py
@@ -4,9 +4,9 @@ import requests
 
 
 router = APIRouter()
-base_url = os.getenv("INVENTORY_BASE_URL")
-bearer_token = os.getenv("INVENTORY_TOKEN")
-# See https://snipe-it.readme.io/reference for full Snipe-IT API reference
+base_url = os.getenv("SUPPORT_BASE_URL")
+bearer_token = os.getenv("SUPPORT_TOKEN")
+# See https://docs.zammad.org/en/latest/api/intro.html for full Zammad API reference
 
 
 @router.get('/users')
@@ -18,7 +18,5 @@ async def users():
         raise HTTPException(
             status_code=get_users_response.status_code,
             detail="General Error")
-    users_list = get_users_response.json()["rows"]
-    usernames = list(map(lambda user: user["username"], users_list))
-    patient_ids = list(filter(lambda name: name[1] == "-", usernames))
-    return patient_ids
+    users_list = get_users_response.json()
+    return users_list
