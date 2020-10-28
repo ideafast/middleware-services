eb704eecc7b87ea22525dff7f4d62e580a23d4ff
Drew Miley <drew.miley@newcastle.ac.uk>
2020-10-28 12:54:54 +0000

    Adds ACCESS_AUTHENTICATED_ENDPOINTS env variable

--- consumer/main.py
+++ consumer/main.py
@@ -7,13 +7,15 @@ from consumer.routers import auth, devices, inventory, support
 from consumer.utils.general import CustomResponse
 
 load_dotenv(find_dotenv('.consumer.env'))
+access_authenticated_endpoints = os.getenv("ACCESS_AUTHENTICATED_ENDPOINTS")
 module_name = os.getenv("MODULE_NAME")
 variable_name = os.getenv("VARIABLE_NAME")
 consumer = FastAPI(default_response_class=CustomResponse)
 consumer.include_router(auth.router)
 consumer.include_router(devices.router)
-consumer.include_router(inventory.router, prefix="/inventory")
-consumer.include_router(support.router, prefix="/support")
+if bool(access_authenticated_endpoints):
+    consumer.include_router(inventory.router, prefix="/inventory")
+    consumer.include_router(support.router, prefix="/support")
 
 
 def main():
