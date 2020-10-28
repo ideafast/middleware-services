4f1fe9e4ba869b5fdb566d50f4bbb35431300830
Jay Rainey <jawrainey@gmail.com>
2020-10-28 14:16:30 +0000

    Update main.py

--- consumer/main.py
+++ consumer/main.py
@@ -7,13 +7,17 @@ from consumer.routers import auth, devices, inventory, support
 from consumer.utils.general import CustomResponse
 
 load_dotenv(find_dotenv('.consumer.env'))
+
 access_authenticated_endpoints = os.getenv("ACCESS_AUTHENTICATED_ENDPOINTS")
 module_name = os.getenv("MODULE_NAME")
 variable_name = os.getenv("VARIABLE_NAME")
+
 consumer = FastAPI(default_response_class=CustomResponse)
+
 consumer.include_router(auth.router)
 consumer.include_router(devices.router)
-if bool(access_authenticated_endpoints):
+
+if access_authenticated_endpoints:
     consumer.include_router(inventory.router, prefix="/inventory")
     consumer.include_router(support.router, prefix="/support")
 
