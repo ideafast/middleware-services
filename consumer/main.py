28065769c591921ea72d23467fe135449689eb2e
Drew Miley <drew.miley@newcastle.ac.uk>
2020-10-28 14:20:33 +0000

    Zammad connection in consumer API (#5)

--- consumer/main.py
+++ consumer/main.py
@@ -1,21 +1,30 @@
 import uvicorn
+import os
 from fastapi import FastAPI
 from dotenv import load_dotenv, find_dotenv
 
-from consumer.routers import devices, auth, inventory
+from consumer.routers import auth, devices, inventory, support
 from consumer.utils.general import CustomResponse
 
+load_dotenv(find_dotenv('.consumer.env'))
 
-load_dotenv(find_dotenv())
+access_authenticated_endpoints = os.getenv("ACCESS_AUTHENTICATED_ENDPOINTS")
+module_name = os.getenv("MODULE_NAME")
+variable_name = os.getenv("VARIABLE_NAME")
+
 consumer = FastAPI(default_response_class=CustomResponse)
-consumer.include_router(devices.router)
+
 consumer.include_router(auth.router)
-consumer.include_router(inventory.router, prefix="/inventory")
+consumer.include_router(devices.router)
+
+if access_authenticated_endpoints:
+    consumer.include_router(inventory.router, prefix="/inventory")
+    consumer.include_router(support.router, prefix="/support")
 
 
 def main():
     uvicorn.run(
-        "consumer.main:consumer",
+        f"{module_name}:{variable_name}",
         host="0.0.0.0",
         port=8000,
         reload=True)
---
+++ consumer/routers/support.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, HTTPException
+import os
+import requests
+
+
+router = APIRouter()
+base_url = os.getenv("SUPPORT_BASE_URL")
+bearer_token = os.getenv("SUPPORT_TOKEN")
+# See https://docs.zammad.org/en/latest/api/intro.html for full Zammad API reference
+
+
+@router.get('/users')
+async def users():
+    get_users_response = requests.get(
+        f"{base_url}users",
+        headers={"Authorization": f'Bearer {bearer_token}'})
+    if 400 <= get_users_response.status_code < 500:
+        raise HTTPException(
+            status_code=get_users_response.status_code,
+            detail="General Error")
+    users_list = get_users_response.json()
+    usernames = list(map(lambda user: f"{user['firstname']} {user['lastname']}", users_list))
+    return usernames
