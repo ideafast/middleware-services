7b4c5c4856ef63cfed27ae333f8d7513459a928a
Drew Miley <drew.miley@newcastle.ac.uk>
2020-10-29 13:42:27 +0000

    Loads env variables in dmp file

--- data_transfer/jobs/dmp.py
+++ data_transfer/jobs/dmp.py
@@ -5,10 +5,19 @@ import requests
 
 router = APIRouter()
 base_url = os.getenv("DMP_BASE_URL")
+username = os.getenv("DMP_USERNAME")
+password = os.getenv("DMP_PASSWORD")
+totp = os.getenv("DMP_TOTP")
 
 
 @router.get('/studies')
 async def studies():
+    login_response = requests.get(
+        f"{base_url}graphql")
+    if 400 <= login_response.status_code < 500:
+        raise HTTPException(
+            status_code=login_response.status_code,
+            detail="General Error")
     return 'STUDIES'
 
 
