8253f7448adcca340101dcacf5eb35ec8f61c42d
Jay Rainey <jawrainey@gmail.com>
2020-10-29 14:03:30 +0000

    Prompt for user input if rebuilding same image

--- cli.py
+++ cli.py
@@ -5,9 +5,9 @@ import click
 REGISTRY = 'ideafast/middleware'
 
 
-def run_command(command: str) -> None:
+def run_command(command: str, capture: bool = False) -> None:
     """Helper method to run shell commands"""
-    subprocess.run([command], shell=True)
+    return subprocess.run([command], shell=True, capture_output=capture)
 
 
 def git_tag_local(version: str) -> None:
@@ -20,6 +20,14 @@ def git_push_tag_remote(version: str) -> None:
     run_command(f"git push origin {version}")
 
 
+def docker_image_exists(version: str) -> str:
+    """Check if docker image exists based on version."""
+    command = f'docker images -q {REGISTRY}:{version}'
+    res = run_command(command, True)
+    image_hash = res.stdout.decode('ascii').rstrip()
+    return image_hash
+
+
 @click.group()
 def cli():
     """CLI for building and deploying middleware in Docker."""
@@ -29,6 +37,9 @@ def cli():
 @click.argument('version')
 def build(version: str) -> None:
     """Build docker image."""
+    message = "THIS VERSION ALREADY EXISTS.\nDo you want to rebuild it?"
+    if docker_image_exists(version):
+        click.confirm(message, abort=True)
     git_tag_local(version)
     run_command(f"docker build -t {REGISTRY}:{version} .")
 
