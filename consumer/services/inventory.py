207572c5e29e6d40851bce9a0ba63c6f3f76106e
Jay Rainey <jawrainey@gmail.com>
2021-01-28 10:33:34 +0000

    Throw when rate limit met on inventory API

--- consumer/services/inventory.py
+++ consumer/services/inventory.py
@@ -11,6 +11,11 @@ async def response(path: str, params: str = None) -> json:
     url = f"{config.inventory_base_url}/{path}"
     res = requests.get(url, params=params, headers=headers)
 
+    if res.status_code == 429:
+        raise CustomException(
+            errors=['Rate Limit on API access.'],
+            status_code=429)
+
     # snipe-it returns a login page (html) on error
     if 'text/html' in res.headers.get('content-type', ''):
         raise CustomException(
