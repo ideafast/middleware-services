3b1555b8c409c5c9d0e2cb2663985d7f964dc164
Jay Rainey <jawrainey@gmail.com>
2021-02-01 09:29:10 +0000

    Minor comments and removed print statements

--- data_transfer/services/ucam.py
+++ data_transfer/services/ucam.py
@@ -1,8 +1,8 @@
 from data_transfer.config import config
 from data_transfer.utils import format_weartime, read_csv_from_cache
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 from datetime import datetime
-from pathlib import Path
+from typing import Optional
 
 
 @dataclass
@@ -11,7 +11,7 @@ class UCAMPayload:
     UCAM uses different field naming than our middleware. This Payload
     Creates consistency between how Records are used
 
-    Current return values:
+    Current return values from UCAM DB:
         SubjectID, Deviations, DeviceID, 
         VTTGeneratedParticipantID, StartDate, 
         EndDate, SubjectGroup
@@ -52,13 +52,14 @@ class PatientRecord:
 
 def __get_patients() -> [UCAMPayload]:
     """
-    All records from the UCAM.
+    All records from the UCAM DB.
     
     GET /patients/
     """
     def __create_record(data: dict) -> UCAMPayload:
         """
-        Convenient method to serialise payload
+        Convenient method to serialise payload.
+        Creates mapping between UCAM and our intended use.
         """
         return UCAMPayload(
             device_id=data['DeviceID'],
@@ -73,13 +74,19 @@ def __get_patients() -> [UCAMPayload]:
     return [__create_record(d) for d in read_csv_from_cache(config.ucam_data)]
 
 
-def record_by_patient_id(patient_id: str) -> PatientRecord:
+def get_record(patient_id: str) -> Optional[PatientRecord]:
     """
     Transforms the payload for consistent access with Record
 
     GET /patients/<patient_id>/
     """
     patient_records = [r for r in __get_patients() if r.patient_id == patient_id]
+    
+    # No records exist for that patient, 
+    # e.g., if a device was not worn or a staff member forget to add the record
+    if len(patient_records) == 0:
+        return None
+
     patient = patient_records[0]
     patient = Patient(patient.patient_id, patient.disease)
 
@@ -100,10 +107,13 @@ def record_by_patient_id(patient_id: str) -> PatientRecord:
 
 
 def device_history(device_id: str) -> [UCAMPayload]:
+    """
+    A device may be worn by many patients. This returns such history.
+    """
     return [r for r in __get_patients() if r.device_id == device_id]
 
 
-def record_id_by_data_creation(device_id: str, start_wear: datetime, end_wear: datetime) -> UCAMPayload:
+def record_by_wear_period(device_id: str, start_wear: datetime, end_wear: datetime) -> Optional[UCAMPayload]:
     """
     If data was created on a certain period then it belongs to an individual patient.
     """
@@ -112,22 +122,5 @@ def record_id_by_data_creation(device_id: str, start_wear: datetime, end_wear: d
     for record in device_wear_periods:
         within_start_period = start_wear >= record.start_wear <= record.end_wear
         within_end_period = record.start_wear <= end_wear <= record.end_wear
-
         if within_start_period and within_end_period:
             return record
-    
-
-
-# specific information about device
-patient_record = record_by_patient_id('K-ZH4ZYY')
-print(patient_record)
-
-# i.e., all patients that used a specific device
-# we can then use wear period to determine use ...
-history = device_history('DRM-DAX2S4')
-print (history)
-
-# i.e., if a device is used by multiple patients and
-# we know when data was created we can determine patient ID
-record_found = record_id_by_data_creation('DRM-DAX2S4', format_weartime('29/10/2020', 'ucam'), format_weartime('31/10/2020', 'ucam'))
-print (record_found.patient_id)
