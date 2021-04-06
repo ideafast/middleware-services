f9267c5a3b7675f95243e8d44440efa26886f94c
Jay Rainey <jawrainey@gmail.com>
2021-04-06 08:40:00 +0100

    Refactor inventory tests
    - Use patch with test_date_within and test_date_outside
    - Rename those methods to mirror intended use
    - Added test_device_history_success/failure
    - Moved test_normalise_day to utils
    - Renamed mock_inventory_requests to mirror usage and removed requests_mock from inventory tests as we don't use status_code, etc

--- tests/data_transfer_tests/services/conftest.py
+++ tests/data_transfer_tests/services/conftest.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 
 import pytest
-import requests
-import requests_mock
 
 from data_transfer import utils
-from data_transfer.config import config
 from data_transfer.services import ucam
 
 folder = Path(__file__).parent
@@ -20,27 +17,10 @@ def mock_data() -> None:
 
 
 @pytest.fixture(scope="function")
-def mock_inventory_devices_bytype() -> dict:
-    return utils.read_json(Path(f"{folder}/data/inventory/mock_devices_bytype.json"))
+def mock_inventory_devices_bytype_response() -> dict:
+    return utils.read_json(Path(f"{folder}/data/inventory/devices_bytype.json"))
 
 
-@pytest.fixture
-def mock_inventory_requests() -> dict:
-    inventory_history_response = utils.read_json(
-        Path(f"{folder}/data/inventory/mock_history.json")
-    )
-    session = requests.Session()
-    adapter = requests_mock.Adapter()
-
-    config.inventory_api = f"mock://{config.inventory_api[len('http://'):]}"
-
-    # mocks _device_history
-    get_history = adapter.register_uri(
-        "GET",
-        f"{config.inventory_api}device/history/BTF-123456",
-        json=inventory_history_response,
-        status_code=200,
-    )
-
-    session.mount("mock://", adapter)
-    return {"session": session, "get_history": get_history}
+@pytest.fixture(scope="function")
+def mock_inventory_history_response() -> dict:
+    return utils.read_json(Path(f"{folder}/data/inventory/history.json"))
