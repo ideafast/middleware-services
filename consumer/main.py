6489ae6d671e6fdafda885ffc6510f1f9a721ab1
Jay Rainey <jawrainey@gmail.com>
2021-04-13 11:21:50 +0100

    Catch requests errors (#75)
    * Default outline for catching requests
    
    * Default status code is 500
    
    * Pull out error response to shared method

--- consumer/main.py
+++ consumer/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from requests import RequestException
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from .routers import auth, devices, inventory, support
-from .utils.errors import CustomException, custom_error_handler, http_error_handler
+from .utils.errors import (
+    CustomException,
+    custom_error_handler,
+    http_error_handler,
+    http_error_handler_requests,
+)
 from .utils.general import CustomResponse
 
 consumer = FastAPI(default_response_class=CustomResponse)
@@ -14,4 +20,5 @@ consumer.include_router(inventory.router, prefix="/inventory")
 consumer.include_router(support.router, prefix="/support")
 
 consumer.add_exception_handler(StarletteHTTPException, http_error_handler)
+consumer.add_exception_handler(RequestException, http_error_handler_requests)
 consumer.add_exception_handler(CustomException, custom_error_handler)
